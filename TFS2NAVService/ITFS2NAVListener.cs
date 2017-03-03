731a92eb0c11cb4239b7954884e1e382a7ef1d64
KSACEK <kamil.sacek@navertica.com>
2017-03-03 07:50:01 +0100

    feat(TFS2NAV): First version

---
+++ TFS2NAVService/ITFS2NAVListener.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Kine
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ITFS2NAVListener
+    {
+
+        [OperationContract]
+        [WebInvoke(BodyStyle = WebMessageBodyStyle.Wrapped, ResponseFormat = WebMessageFormat.Json)]
+        string ToNAV(Stream body);
+
+    }
+
+}
---
+++ TFS2NAVService/TFS2NAVListener.svc.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Kine
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class TFS2NAVListener : ITFS2NAVListener
+    {
+        public string ToNAV(Stream body)
+        {
+            var reader = new StreamReader(body);
+            //var p = JsonConvert.DeserializeObject<JObject>(reader.ReadToEnd());
+            var p = reader.ReadToEnd();
+            reader.Close();
+            reader.Dispose();
+            
+            string result = PassDataToNAV(p);
+            return (result);
+        }
+
+        string PassDataToNAV(string data)
+        {
+            var navPort = new NAV.TFS2NAV_PortClient();
+            if (!String.IsNullOrEmpty(Kine.Properties.Settings.Default.WSUserName))
+            {
+                navPort.ClientCredentials.UserName.Password = Properties.Settings.Default.WSUserPwd;
+                navPort.ClientCredentials.UserName.UserName = Properties.Settings.Default.WSUserName;
+            } else
+            {
+                navPort.ClientCredentials.Windows.ClientCredential = (NetworkCredential)System.Net.CredentialCache.DefaultCredentials;
+            }
+            var result = navPort.TFS2NAV(data);
+            return (result);
+        }
+    }
+}
